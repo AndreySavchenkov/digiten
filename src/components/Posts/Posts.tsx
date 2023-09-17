8794ecb42ceac7eb5564766986d7e7b08cfe2b98
AndreySavchenkov <sannndvik16@gmail.com>
2023-09-17 13:39:25 +0200

    feature: added no found block, and changed styles

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -8,6 +8,16 @@ type PostsProps = {
 };
 
 export const Posts: FC<PostsProps> = ({ posts }) => {
+  //pattern - Early Return
+  if (!posts || posts.length === 0) {
+    return (
+      <div className={styles.container}>
+        <span>Nothing found :(</span>
+        <span> Try changing the filter values</span>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.posts}>
       {posts.map((post, index) => (
